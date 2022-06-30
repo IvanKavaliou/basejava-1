125d98764583887da5269b75491e7f067350c757
Ivan Kavaliou <luckucat@gmail.com>
2022-06-30 19:52:51 +0200

    HW 13 pt 1

--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -1,11 +1,13 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.sql.ConnectionFactory;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SqlStorage implements Storage {
@@ -42,34 +44,72 @@ public class SqlStorage implements Storage {
 
     @Override
     public void update(Resume r) {
-
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement("UPDATE resume SET full_name =? WHERE uuid=?")) {
+            ps.setString(1, r.getFullName());
+            ps.setString(2, r.getUuid());
+            if (ps.executeUpdate() <=0){
+                throw new NotExistStorageException(r.getUuid());
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 
     @Override
     public void save(Resume r) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)")) {
-            ps.setString(1, r.getUuid());
-            ps.setString(2, r.getFullName());
-            ps.execute();
+            try {
+                get(r.getUuid());
+                throw new ExistStorageException(r.getUuid());
+            } catch (NotExistStorageException e){
+                ps.setString(1, r.getUuid());
+                ps.setString(2, r.getFullName());
+                ps.execute();
+            }
         } catch (SQLException e) {
             throw new StorageException(e);
         }
-
     }
 
     @Override
     public void delete(String uuid) {
-
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement("DELETE FROM resume WHERE uuid =?")) {
+            ps.setString(1, uuid);
+            if (ps.executeUpdate() <=0){
+                throw new NotExistStorageException(uuid);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 
     @Override
     public List<Resume> getAllSorted() {
-        return null;
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement("SELECT * FROM resume")) {
+            ResultSet rs = ps.executeQuery();
+            List<Resume> result = new ArrayList<>();
+            while (rs.next()){
+                result.add(new Resume(rs.getString("uuid").trim(), rs.getString("full_name")));
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM resume")) {
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 }
---
+++ test/ru/javawebinar/basejava/storage/SqlStorageTest.java
@@ -0,0 +1,12 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.Config;
+
+public class SqlStorageTest extends AbstractStorageTest{
+    private static Config config = Config.get();
+    public SqlStorageTest() {
+        super(new SqlStorage(config.getUrl(),
+                config.getUser(),
+                config.getPassword()));
+    }
+}
